aa01cbe609dd517d5b50d6b75db9dcbfe4b7c9f2
Lucas-Bento-D <lucasbentoskl@outlook.com>
2025-11-03 23:29:52 -0300

    feat: add gauge metric example

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ const counter = new promClient.Counter({
     labelNames: ["statusCode"]
 })
 
+const gauge = new promClient.Gauge({
+    name:"requests_gauge",
+    help: "exemplo de gauge"
+})
+
 app.get("/", (req, res) => {
     counter.labels(200).inc()
     counter.labels(300).inc()
+    gauge.set(100*Math.random())
     res.send("Hello Prometheus")
 })
 
